9d4d97bd5c249e524ab5c3b3f1d81e1fb9c52149
toshiyuki <toshiyuki.saito.16@gmail.com>
2020-05-27 09:05:46 +0900

    #02 first implements and test

---
+++ temp.s
@@ -0,0 +1,5 @@
+.intel_syntax noprefix
+.globl main
+main:
+  mov rax, 123
+  ret
---
+++ test.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+assert() {
+  expected="$1"
+  input="$2"
+
+  ./main "$input" > tmp.s
+  cc -o tmp tmp.s
+  ./tmp
+  actual="$?"
+
+  if [ "$actual" = "$expected" ]; then
+    echo "$input => $actual"
+  else
+    echo "$input => $expected expedted, but got $actual"
+    exit 1
+  fi
+}
+
+assert 0 0
+assert 42 42
+
+echo OK
