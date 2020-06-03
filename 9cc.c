70f40939088cfb3dd1c874ee606e05cee08c4b30
toshiyuki <toshiyuki.saito.16@gmail.com>
2020-06-03 09:11:14 +0900

    #07 implements BNF

--- 9cc.c
+++ 9cc.c
@@ -11,6 +11,23 @@ typedef enum {
   TK_EOF, // 入力の終わりを表すトークン
 } TokenKind;
 
+typedef enum {
+  ND_ADD,
+  ND_SUB,
+  ND_MUL,
+  ND_DIV,
+  ND_NUM,
+} NodeKind;
+
+typedef struct Node Node;
+
+struct Node {
+  NodeKind kind;
+  Node *lhs;
+  Node *rhs;
+  int val;
+};
+
 typedef struct Token Token;
 
 struct Token {
@@ -101,6 +118,59 @@ Token *tokenize() {
 
   new_token(TK_EOF, cur, p);
   return head.next;
+}
+
+Node *new_node(NodeKind kind, Node *lhs, Node *rhs) {
+  Node *node = calloc(1, sizeof(Node));
+  node->kind = kind;
+  node->lhs = lhs;
+  node->rhs = rhs;
+  return node;
+}
+
+Node *new_node_num(int val) {
+  Node *node = calloc(1, sizeof(Node));
+  node->kind = ND_NUM;
+  node->val = val;
+  return node;
+}
+
+// expr    = mul ("+" mul | "-" mul)*
+Node *expr() {
+  Node *node = mul();
+  for (;;) {
+    if (consume('+')) {
+      node = new_node(ND_ADD, node, mul());
+    } else if (consume('-')) {
+      node = new_node(ND_SUB, node, mul());
+    } else {
+      return node;
+    }
+  }
+}
+
+// mul     = primary ("*" primary | "/" primary)*
+Node *mul() {
+  Node *node = primary();
+  for (;;) {
+    if (consume('*')) {
+      node = new_node(ND_MUL, node, primary());
+    } else if (consume('/')) {
+      node = new_node(ND_DIV, node, primary());
+    } else {
+      return node;
+    }
+  }
+}
+
+// primary = num | "(" expr ")"
+Node *primary() {
+  if (consume('(')) {
+    Node *node = expr();
+    expect(')');
+    return node;
+  }
+  return new_node_num(expect_number());
 }
 
 int main(int argc, char **argv) {
