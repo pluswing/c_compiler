d7330c4d32d5c72293fd0a376bece72e22b61d4a
toshiyuki <toshiyuki.saito.16@gmail.com>
2020-06-23 08:10:05 +0900

    #21 return syntax (2)

--- codegen.c
+++ codegen.c
@@ -185,6 +185,10 @@ void gen(Node *node) {
   switch (node->kind) {
   case ND_RETURN:
     gen(node->lhs);
+    printf("  pop rax\n");
+    printf("  mov rsp, rbp\n");
+    printf("  pop rbp\n");
+    printf(" ret\n");
     return;
   case ND_NUM:
     printf("  push %d\n", node->val);
