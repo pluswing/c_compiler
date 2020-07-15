055241a2531d4277b9cbe2966bac33c7fb1dc5f2
toshiyuki <toshiyuki.saito.16@gmail.com>
2020-07-16 08:09:08 +0900

    #38 fix tests

--- test.sh
+++ test.sh
@@ -42,99 +42,112 @@ sum(n) {
 }
 "
 
-# assert 42 "42;"
-# assert 21 "5+20-4;"
-# assert 41 " 12 + 34 - 5 ;"
-# assert 47 "5+6*7;"
-# assert 15 "5*(9-6);"
-# assert 4 "(3+5)/2;"
-# assert 10 "-10+20;"
-# assert 10 "- -10;"
-# assert 10 "- - +10;"
+assert 42 "main() return 42;"
+assert 21 "main() return 5+20-4;"
+assert 41 "main() return  12 + 34 - 5 ;"
+assert 47 "main() return 5+6*7;"
+assert 15 "main() return 5*(9-6);"
+assert 4 "main() return (3+5)/2;"
+assert 10 "main() return -10+20;"
+assert 10 "main() return - -10;"
+assert 10 "main() return - - +10;"
 
-# # compare tests(1)
-# assert 0 "0==1;"
-# assert 1 "42==42;"
-# assert 1 "0!=1;"
-# assert 0 "42!=42;"
+# compare tests(1)
+assert 0 "main() return 0==1;"
+assert 1 "main() return 42==42;"
+assert 1 "main() return 0!=1;"
+assert 0 "main() return 42!=42;"
 
-# # compare tests(2)
-# assert 1 "0<1;"
-# assert 0 "1<1;"
-# assert 0 "2<1;"
-# assert 1 "0<=1;"
-# assert 1 "1<=1;"
-# assert 0 "2<=1;"
+# compare tests(2)
+assert 1 "main() return 0<1;"
+assert 0 "main() return 1<1;"
+assert 0 "main() return 2<1;"
+assert 1 "main() return 0<=1;"
+assert 1 "main() return 1<=1;"
+assert 0 "main() return 2<=1;"
 
-# # compare tests(3)
-# assert 1 "1>0;"
-# assert 0 "1>1;"
-# assert 0 "1>2;"
-# assert 1 "1>=0;"
-# assert 1 "1>=1;"
-# assert 0 "1>=2;"
+# compare tests(3)
+assert 1 "main() return 1>0;"
+assert 0 "main() return 1>1;"
+assert 0 "main() return 1>2;"
+assert 1 "main() return 1>=0;"
+assert 1 "main() return 1>=1;"
+assert 0 "main() return 1>=2;"
 
-# # variable tests
-# assert 14 "a = 3;
-# b = 5 * 6 - 8;
-# a + b / 2;"
-# assert 6 "foo = 1;
-# bar = 2 + 3;
-# foo + bar;"
+# variable tests
+assert 14 "main() {a = 3;
+b = 5 * 6 - 8;
+a + b / 2;
+}"
+assert 6 "main() {foo = 1;
+bar = 2 + 3;
+foo + bar;
+}"
 
-# # return tests
-# assert 14 "a = 3;
-# b = 5 * 6 - 8;
-# return a + b / 2;"
-# assert 5 "return 5;
-# return 8;"
+# return tests
+assert 14 "main() {a = 3;
+b = 5 * 6 - 8;
+return a + b / 2;
+}"
+assert 5 "main() {return 5;
+return 8;
+}"
 
-# # IF tests
-# assert 3 "a = 3;
-# if (a == 3) return a;
-# return 5;
-# "
-# assert 5 "if (3 != 3) return 1;
-# return 5;
-# "
-# assert 5 "if (3 != 3) return 1;
-# else return 5;
-# return 2;
-# "
+# IF tests
+assert 3 "main() {a = 3;
+if (a == 3) return a;
+return 5;
+}
+"
+assert 5 "main() {if (3 != 3) return 1;
+return 5;
+}
+"
+assert 5 "main() {if (3 != 3) return 1;
+else return 5;
+return 2;
+}
+"
 
-# # while
-# assert 11 "i = 0;
-# while (i <= 10) i = i + 1;
-# return i;"
+# while
+assert 11 "main() { i = 0;
+while (i <= 10) i = i + 1;
+return i;
+}"
 
-# # for
-# assert 30 "a = 0;
-# for (i = 0; i < 10; i = i + 1) a = a + 2;
-# return i + a;"
-# assert 10 "a = 0;
-# for (;a < 10;) a = a + 1;
-# return a;"
+# for
+assert 30 "main() {a = 0;
+for (i = 0; i < 10; i = i + 1) a = a + 2;
+return i + a;
+}"
+assert 10 "main() {a = 0;
+for (;a < 10;) a = a + 1;
+return a;
+}"
 
-# # multi contorl syntax
-# assert 6 "a = 3;
-# if (a == 1) return 4;
-# if (a == 2) return 5;
-# if (a == 3) return 6;"
-# assert 10 "a = 0;
-# for(;;a = a + 1) if (a == 5) return 10;
-# return 2;"
+# multi contorl syntax
+assert 6 "main() {a = 3;
+if (a == 1) return 4;
+if (a == 2) return 5;
+if (a == 3) return 6;
+}"
+assert 10 "main() {a = 0;
+for(;;a = a + 1) if (a == 5) return 10;
+return 2;
+}"
 
-# # block
-# assert 10 "a = 0;
-# for(;;) {
-#   a = a + 1;
-#   if (a == 5) return 10;
-# }
-# return 2;"
+# block
+assert 10 "main() {a = 0;
+for(;;) {
+  a = a + 1;
+  if (a == 5) return 10;
+}
+return 2;
+}"
 
-# # func
-# assert 0 "foo();"
-# assert 0 "bar(3, 4);"
-# assert 0 "bar2(3, 4, 5);"
+# func
+assert 1 "main() return foo();"
+assert 7 "main() return bar(3, 4);"
+assert 12 "main() return bar2(3, 4, 5);"
 
 echo OK
