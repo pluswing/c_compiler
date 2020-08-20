2ff9ffa82eb441e7586f821a80b94d8d4390a44e
toshiyuki <toshiyuki.saito.16@gmail.com>
2020-08-21 07:46:28 +0900

    #59 implement fileread and improve error_at

--- tokenize.c
+++ tokenize.c
@@ -2,6 +2,31 @@
 
 Token *token;
 char *user_input;
+char *filename;
+
+char *read_file(char *path) {
+  FILE *fp = fopen(path, "r");
+  if (!fp) {
+    error("cannot open %s: %s", path, strerror(errno));
+  }
+  if (fseek(fp, 0, SEEK_END) == -1) {
+    error("%s: fseek %s", path, strerror(errno));
+  }
+  size_t size = ftell(fp);
+  if (fseek(fp, 0, SEEK_SET) == -1) {
+    error("%s: fseek %s", path, strerror(errno));
+  }
+
+  char *buf = calloc(1, size + 2);
+  fread(buf, size, 1, fp);
+
+  if (size == 0 || buf[size - 1] != '\n') {
+    buf[size++] = '\n';
+  }
+  buf[size] = '\0';
+  fclose(fp);
+  return buf;
+}
 
 void error(char *fmt, ...) {
   va_list ap;
@@ -15,8 +40,26 @@ void error_at(char *loc, char *fmt, ...) {
   va_list ap;
   va_start(ap, fmt);
 
-  int pos = loc - user_input;
-  fprintf(stderr, "%s\n", user_input);
+  char *line = loc;
+  while(user_input < line && line[-1] != '\n') {
+    line--;
+  }
+
+  char *end = loc;
+  while (*end != '\n') {
+    end++;
+  }
+
+  int line_num = 1;
+  for (char *p = user_input; p < line; p++) {
+    if (*p == '\n') {
+      line_num++;
+    }
+  }
+
+  int indent = fprintf(stderr, "%s:%d: ", filename, line_num);
+  fprintf(stderr, "%.*s\n", (int)(end - line), line);
+  int pos = loc - line + indent;
   fprintf(stderr, "%*s", pos, "");
   fprintf(stderr, "^ ");
   vfprintf(stderr, fmt, ap);
